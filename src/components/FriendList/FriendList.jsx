185f24a771887a09a717d604a574231f55a51c44
KL-ML <knataly0513@gmail.com>
2025-05-09 21:55:02 +0300

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,16 +1,19 @@
 import PropTypes from 'prop-types';
 import FriendListItem from './FriendListItem/FriendListItem';
-import css from "./FriendList.module.css"
+import css from './FriendList.module.css';
+import Container from '../components-structure/Container/Container';
 
 export default function FriendList({ friends }) {
   return (
-    <ul className={css.friendsList}>
-      {friends.map(({ avatar, name, isOnline, id }) => (
-        <li key={id}>
-          <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
-        </li>
-      ))}
-    </ul>
+    <Container variant={'friendsListContainer'}>
+      <ul className={css.friendsList}>
+        {friends.map(({ avatar, name, isOnline, id }) => (
+          <li key={id}>
+            <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
+          </li>
+        ))}
+      </ul>
+    </Container>
   );
 }
 
