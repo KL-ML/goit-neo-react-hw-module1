f5496e8b98210497bcb7214ffa99078a6d8bdff7
KL-ML <knataly0513@gmail.com>
2025-05-09 21:55:33 +0300

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,30 +1,32 @@
 import PropTypes from 'prop-types';
-import css from "./TransactionHistory.module.css"
+import css from './TransactionHistory.module.css';
+import Container from '../components-structure/Container/Container';
 
 export default function TransactionHistory({ items }) {
   return (
-    <table>
-      <thead className={css.tableHead}>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        {items.map(({ id, type, amount, currency }) => (
-          <tr className={css.tableRow} key={id}>
-            <td>{type}</td>
-            <td>{amount}</td>
-            <td>{currency}</td>
+    <Container variant={'tableContainer'}>
+      <table className={css.tableWrap}>
+        <thead className={css.tableHead}>
+          <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+
+        <tbody>
+          {items.map(({ id, type, amount, currency }) => (
+            <tr className={css.tableRow} key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Container>
   );
 }
-
 
 TransactionHistory.PropTypes = {
   items: PropTypes.arrayOf(PropTypes.object).isRequired,
